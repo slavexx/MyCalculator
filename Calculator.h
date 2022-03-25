4349ef1fa63d560f7b5ed407f8beb51f4496e855
Viachaslau Lepeshau <slavex001@tut.by>
2022-03-25 17:09:57 +0300

    Reorganized the header

--- Calculator.h
+++ Calculator.h
@@ -1,19 +1,19 @@
 #pragma once
-#pragma once
 
 #include <QWidget>
 #include <qlabel.h>
 #include <qpushbutton.h>
 
 class Calculator : public QWidget {
+    Q_OBJECT
 public:
     Calculator(QWidget* parent = 0);
 private:
     QLabel* label;
     QPushButton btn[20];
     bool pressedOperation = false;
-private slots:
-    void digitClicked();
     double rightOperand = 0.0;
     double leftOperand = 0.0;
+private slots:
+    void digitClicked();
 };
